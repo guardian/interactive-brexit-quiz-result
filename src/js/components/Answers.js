74cdcab1c6cc5f48a4b0cc582ee7ff899ff93333
carlo <littleark@gmail.com>
2016-06-07 14:59:57 +0100

    switched to waaaaay off

--- src/js/components/Answers.js
+++ src/js/components/Answers.js
@@ -24,10 +24,10 @@ export default function Answers(data,options) {
 
 	console.log(data,options);
 //var clrScale = { name: 'newsSupport2', selected: '#c5ffff', scale: ['#000026','#00284e','#00507b','#197caa','#5babdc','#90ddff','#c5ffff'] };
-    let scale=scaleLinear().range([0,100]).domain([-0.5,0.5]),
-        scaleWidth=scaleLinear().range([0,100]).domain([0,0.5]),
-        colorScaleOver=scaleQuantize().range((['#00507b','#197caa','#5babdc','#90ddff','#c5ffff']).reverse()).domain([0,0.4]),
-        colorScaleUnder=scaleQuantize().range((['#9c0000','#d61d00','#ff5b32','#ff8f5f','#ffc38e']).reverse()).domain([0,0.4]);
+    // let scale=scaleLinear().range([0,100]).domain([-0.5,0.5]),
+    //     scaleWidth=scaleLinear().range([0,100]).domain([0,0.5]),
+    //     colorScaleOver=scaleQuantize().range((['#00507b','#197caa','#5babdc','#90ddff','#c5ffff']).reverse()).domain([0,0.4]),
+    //     colorScaleUnder=scaleQuantize().range((['#9c0000','#d61d00','#ff5b32','#ff8f5f','#ffc38e']).reverse()).domain([0,0.4]);
 
     let answers=select(options.container)
                     .append("div")
@@ -52,27 +52,47 @@ export default function Answers(data,options) {
             .classed("under",d=>d.perc.diff<0)
             .html((d,i)=>{
                 let n=(Math.ceil(Math.abs(d.perc.diff)*100)),
-                    char=d.perc.diff>0?"&nbsp;":"&nbsp;",
+                    char="a",//d.perc.diff>0?"&nbsp;":"&nbsp;",
                     word=d.perc.diff>0?"&nbsp;over":"&nbsp;under"
                 //console.log(i,n,d.perc.diff*100)
-                let letters=range(n).map(v=>"<b>"+char+"</b>").join('');
+                
+                let pp=" "+"by "+format(",.2")(Math.abs(d.perc.diff)*100)+" percentage points";
+
+                let letters=range(n-2).map(v=>"<b>"+char+"</b>").join('');
+
+
+                letters="<b>w</b>"+letters+"<b>y</b>";
+                word=" off,"
+
+                if(Math.abs(d.perc.diff)<=0.04) {
+                    letters="<b>o</b><b>f</b><b>f</b><b>&nbsp;</b>";
+                    word=" the money";
+                }
+
+                if(Math.abs(d.perc.diff)<=0.03) {
+                    letters="<b>o</b><b>f</b><b>f</b>";
+                    word=" the money";
+                    pp="";
+                }
+                
+                if(Math.abs(d.perc.diff)<=0.02) {
+                    letters="<b>o</b><b>n</b>";
+                    word=" the money";
+                }
+                if(Math.abs(d.perc.diff)<=0.01) {
+                    letters="<b>o</b>";
+                    word="n the money";
+                }
 
                 
 
-                if(Math.abs(d.perc.diff)<0.1) {
-                    word=" slightly "+word;
-                }
-                if(Math.abs(d.perc.diff)>=0.2) {
-                    word=" badly "+word;
-                }
-
-                let pp="by "+format(",.2")(Math.abs(d.perc.diff)*100)+" percentage points";
+                
 
                 if(d.npp) {
                     pp="";
                 }
 
-                return "<span class=\"num\">"+(i+1)+"</span> <span class=\"question\">"+d.question+"</span><br/>The question was <b>"+letters+"</b><i>"+word+"estimated</i> "+pp+". "+(d.comment || "");
+                return "<span class=\"num\">"+(i+1)+"</span> <span class=\"question\">"+d.question+"</span><br/><span class=\"the-readers\">The readers were </span><b>"+letters+"</b>"+word+pp+". "+(d.comment || "");
             })
             // .select("b")
             //     .style("background-color",d=>{
@@ -85,57 +105,57 @@ export default function Answers(data,options) {
 
 
     return;
-    let line=answer.append("div")
-            .attr("class","line-container")
+    // let line=answer.append("div")
+    //         .attr("class","line-container")
 
-    line.append("span")
-            .attr("class","line")
-            .style("left",d=>{
-                if(d.perc.diff<0) {
-                    return scale(d.perc.diff)+"%"
-                }
-                return scale(0)+"%";
-            })
-            .style("width",d=>{
-                return (Math.abs(scale(d.perc.diff)-50)+0.01)+"%"
-            })
+    // line.append("span")
+    //         .attr("class","line")
+    //         .style("left",d=>{
+    //             if(d.perc.diff<0) {
+    //                 return scale(d.perc.diff)+"%"
+    //             }
+    //             return scale(0)+"%";
+    //         })
+    //         .style("width",d=>{
+    //             return (Math.abs(scale(d.perc.diff)-50)+0.01)+"%"
+    //         })
 
 
-    line.append("span")
-            .attr("class","zero")
-            .style("left",d=>{
-                return scale(0)+"%"
-            })
+    // line.append("span")
+    //         .attr("class","zero")
+    //         .style("left",d=>{
+    //             return scale(0)+"%"
+    //         })
 
-    line.append("span")
-            .attr("class","avg")
-            .style("left",d=>{
-                return scale(d.perc.diff)+"%"
-            })
+    // line.append("span")
+    //         .attr("class","avg")
+    //         .style("left",d=>{
+    //             return scale(d.perc.diff)+"%"
+    //         })
 
-    line.append("span")
-            .attr("class","zero")
-            .style("left",d=>{
-                return scale(0)+"%"
-            })
+    // line.append("span")
+    //         .attr("class","zero")
+    //         .style("left",d=>{
+    //             return scale(0)+"%"
+    //         })
 
-    line.append("p")
-            .classed("right",d=>d.perc.diff<0)
-            .classed("left",d=>d.perc.diff>=0)
-            .style("left",d=>{
-                if(d.perc.diff<0) {
-                    return 0;
-                }
-                return scale(d.perc.diff)+"%"
-            })
-            .style("width",d=>{
-                if(d.perc.diff<0) {
-                    return scale(d.perc.diff)+"%"
-                }
-                return "100%";
-            })
-            .text(d=>{
-                return format("+,.2%")(d.perc.diff);
-            })
+    // line.append("p")
+    //         .classed("right",d=>d.perc.diff<0)
+    //         .classed("left",d=>d.perc.diff>=0)
+    //         .style("left",d=>{
+    //             if(d.perc.diff<0) {
+    //                 return 0;
+    //             }
+    //             return scale(d.perc.diff)+"%"
+    //         })
+    //         .style("width",d=>{
+    //             if(d.perc.diff<0) {
+    //                 return scale(d.perc.diff)+"%"
+    //             }
+    //             return "100%";
+    //         })
+    //         .text(d=>{
+    //             return format("+,.2%")(d.perc.diff);
+    //         })
 
 }
